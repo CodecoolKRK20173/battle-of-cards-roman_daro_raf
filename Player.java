f2e2597352954fd0512ecf72ed61897a338ae194
Rafal Becla <rafal.becla@gmail.com>
2018-08-21 11:01:32 +0200

    Player constructor changed

--- Player.java
+++ Player.java
@@ -2,9 +2,11 @@ public class Player{
     private Pile handPile;
     private Pile stockPile;
     private String name;
+    private int number;
 
-    public Player(String name){
+    public Player(String name, int number){
         this.name = name;
+        this.number = number;
     }
 
 }
