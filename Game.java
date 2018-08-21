e6356009c59ea4d764103b166b6e7b599a9f9c33
Dariusz Domanski <dardomanski@gmail.com>
2018-08-21 13:50:28 +0200

    added loadDeck() method

--- Game.java
+++ Game.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.util.ArrayList;
 
 public class Game{
@@ -5,7 +8,48 @@ public class Game{
     private ArrayList<Player> players;
     private Pile deck;
 
-    public Game(int numberOfPlayers){
+    public Game(int numberOfPlayers, String... names){
+        
+        setPlayers(numberOfPlayers, names);
+        this.deck = new Pile();
+        loadDeck();
+        this.currentPlayer = 1;
+        dealCards();
+    }
+
+    private void setPlayers(int numberOfPlayers, String[] names){
+        for(int i = 1; i <= numberOfPlayers; i++ ){
+            players.add(new Player("Player", i));
+        }
+    }
+
+    private void loadDeck() {
+        final int COUNTRY_NAME_COLLUMN = 0;
+        final int POPULATION_COLLUMN = 1;
+        final int DENSITY_COLLUMN = 2;
+        final int AREA_COLLUMN = 3;
+        final int MEDIAN_AGE_COLLUMN = 4; 
+
+        BufferedReader br = new BufferedReader(new FileReader(new File("countries.txt")));
+
+        String line;
+        while ((line = br.readLine()) != null) {
+            String[] countryData = line.split("/t");
+            deck.addCard(new Card(countryData[COUNTRY_NAME_COLLUMN], 
+                              Float.valueOf(countryData[POPULATION_COLLUMN]), 
+                              Float.valueOf(countryData[DENSITY_COLLUMN]), 
+                              Float.valueOf(countryData[AREA_COLLUMN]), 
+                              Float.valueOf(countryData[MEDIAN_AGE_COLLUMN])));   
+        }
+    }
+
+    private void dealCards(){
 
     }
+
+    public void runGame(){
+
+    }
+
+    
 }
