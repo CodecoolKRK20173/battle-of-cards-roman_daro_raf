5cd365daf9508903aa9cf98518bf39e737417b22
Rafal Becla <rafal.becla@gmail.com>
2018-08-21 12:03:31 +0200

    implemented baisic Game architecture

--- Game.java
+++ Game.java
@@ -5,7 +5,32 @@ public class Game{
     private ArrayList<Player> players;
     private Pile deck;
 
-    public Game(int numberOfPlayers){
+    public Game(int numberOfPlayers, String... names){
+        
+        setPlayers(numberOfPlayers, names);
+        this.deck = loadDeck();
+        this.currentPlayer = 1;
+        dealCards();
+    }
+
+    private void setPlayers(int numberOfPlayers, String[] names){
+        for(int i = 1; i <= numberOfPlayers; i++ ){
+            players.add(new Player("Player", i));
+        }
+    }
+
+    private Pile loadDeck(){
+        // TO DO
+        return null;
+    }
+
+    private void dealCards(){
 
     }
+
+    public void runGame(){
+
+    }
+
+    
 }
