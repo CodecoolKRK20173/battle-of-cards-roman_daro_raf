97784afb2537ce06443071f6976c0b2cfbdb3eab
Dariusz Domanski <dardomanski@gmail.com>
2018-08-21 11:37:23 +0200

    implemented Card class

--- Card.java
+++ Card.java
@@ -1,18 +1,50 @@
 //to be Card
 
-public class Card{
+public class Card {
     private boolean isFaceUp;
     private float area;
     private float population;
-    private float riverLenght;
-    private float mountainHeight;
-    private String riverName;
-    private String mountainName;
+    private float density;
+    private float medianAge;
 
-    public Card(float area, float population, float riverLenght, float mountainHeight, String riverName, String mountainName){
+    public Card(float area, float population, float density, float medianAge) {
+        this.isFaceUp = false;
         this.area = area;
         this.population = population;
-        // TO DO
-    } 
+        this.density = density;
+        this.medianAge = medianAge;
+    }
+
+    public boolean getIsFaceUp() {
+        return this.isFaceUp;
+    }
+
+    public float getArea() {
+        return this.area;
+    }
+
+    public float getPopulation() {
+        return this.population;
+    }
+
+    public float getDensity() {
+        return this.density;
+    }
+
+    public float medianAge() {
+        return this.medianAge;
+    }
+
+    public void setFaceUp() {
+        this.isFaceUp = true;
+    }
+
+    public void setFaceDown() {
+        this.isFaceUp = false;
+    }
+
+    public void flip() {
+        this.isFaceUp = !this.isFaceUp;
+    }
     
 }
