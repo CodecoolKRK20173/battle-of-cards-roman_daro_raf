3df24eb8f05a469bdab787ec7017c2fb63bf5656
Rafal Becla <rafal.becla@gmail.com>
2018-08-21 12:03:03 +0200

    implemented  baisic Player architecture

--- Player.java
+++ Player.java
@@ -1,12 +1,8 @@
-public class Player{
+public abstract class Player{
     private Pile handPile;
     private Pile stockPile;
     private String name;
     private int number;
 
-    public Player(String name, int number){
-        this.name = name;
-        this.number = number;
-    }
 
 }
