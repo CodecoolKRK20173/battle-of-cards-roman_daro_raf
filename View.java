5bb109a9472fd9252d7c1744b1adf7f6ae9cfa78
Dariusz Domanski <dardomanski@gmail.com>
2018-08-22 09:48:55 +0200

    added backSuit() method

--- View.java
+++ View.java
@@ -1,3 +1,23 @@
 public class View{
-    // TO DO
+    final int CARD_WIDTH = 13;
+
+    public String backSuit() {
+        String backSuit = "";
+        String upAndDownRow = "###############";
+
+        backSuit += upAndDownRow;
+        for (int i = 0; i < 5; i++) {
+            backSuit += String.format("#%" + CARD_WIDTH + "s#\n", " ");
+        }
+        backSuit += upAndDownRow;
+
+        return backSuit;
+    }
+
+    public String frontSuit(Card card) {
+        String frontSuit = "";
+        
+
+        return null;
+    }
 }
